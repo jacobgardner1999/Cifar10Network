83b6cdde9521987b1aaaec53bbd82ea5eb34f565
Jacob Gardner <jacob.gardner@bjss.com>
2023-09-26 20:03:01 +0100

    feat: implement data loader

---
+++ cifar_loader.py
@@ -0,0 +1,48 @@
+import os
+
+import pickle
+import numpy as np
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def load_batch(fpath, label_key="labels"):
+    with open(fpath, "rb") as f:
+        d = pickle.load(f, encoding="bytes")
+        d_decoded = {}
+        for k, v in d.items():
+            d_decoded[k.decode("utf8")] = v
+        d = d_decoded
+    data = d["data"]
+    labels = d[label_key]
+
+    data = data.reshape(data.shape[0], 3, 32, 32)
+    return data, labels
+
+def load_data(): 
+    path = './cifar-10-batches-py/'
+
+    num_train_samples = 50000
+
+    x_train = np.empty((num_train_samples, 3, 32, 32), dtype="uint8")
+    y_train = np.empty((num_train_samples,), dtype="uint8")
+
+    for i in range(1, 6):
+        fpath = os.path.join(path, "data_batch_" + str(i))
+        (
+            x_train[(i-1) * 10000 : i * 10000, :, :, :],
+            y_train[(i-1) * 10000 : i * 10000],
+        ) = load_batch(fpath)
+
+    fpath = os.path.join(path, "test_batch")
+    x_test, y_test = load_batch(fpath)
+
+    y_train = np.reshape(y_train, (len(y_train), 1))
+    y_test = np.reshape(y_test, (len(y_test), 1))
+    
+    x_test = x_test.astype(x_train.dtype)
+    y_test = y_test.astype(y_train.dtype)
+
+    return (x_train, y_train), (x_test, y_test)
