f40c70dc7355cecfb2f998568f94c7e35815e066
Jacob Gardner <jacob.gardner@bjss.com>
2023-09-30 17:29:02 +0100

    feat: update to replace lr

--- kerasNetwork.py
+++ kerasNetwork.py
@@ -63,7 +63,7 @@ def define_model2():
                     kernel_initializer='he_uniform'))
     model.add(Dense(10, activation='softmax'))
     # compile model
-    opt = SGD(lr=0.001, momentum=0.9)
+    opt = SGD(learning_rate=0.001, momentum=0.9)
     model.compile(optimizer=opt,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
